7a3d40253aceaf07e8dd9fc55ac457e32514762f
richard-yao <yaoyxsgf@gmail.com>
2017-08-15 14:52:15 +0800

    delete not used import

--- src/main/java/com/richard/java8use/interceptor/MyHandlerInterceptor.java
+++ src/main/java/com/richard/java8use/interceptor/MyHandlerInterceptor.java
@@ -6,7 +6,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
