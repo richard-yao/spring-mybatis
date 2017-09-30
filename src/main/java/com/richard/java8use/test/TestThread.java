f0433f80e2f720c0d1d0178ba659d14327b2b7b1
richard-yao <yaoyxsgf@gmail.com>
2017-09-30 15:40:02 +0800

    1. add a strange example for understand the jdk compile feature

--- src/main/java/com/richard/java8use/test/TestThread.java
+++ src/main/java/com/richard/java8use/test/TestThread.java
@@ -49,3 +49,27 @@ public class TestThread extends Thread {
 		super.run();
 	}
 }
+
+class Test {
+	
+	static {
+		i = 0; // compile successfully
+		i = 3;
+		// System.out.println(i); // will throw error when jvm compile this code because of i not declared
+	}
+	// 通过javap -c 查看编译后的字节码可以知道，static int i; static int j其实是代码最初声明的，因此在上述静态代码块中，直接给i/j赋值并不会产生错误
+	static int i = 1; 
+	static int j = 1;
+	static final int z;  //must be initialized here.
+	static {
+		j = 2;
+		z = 3;
+	}
+
+	public static void main(String[]  args) {
+		//至于在经过static块赋值却未生效，通过字节码可以看到其实i = 0, i = 3的赋值是有操作的，但是最后执行的是i = 1操作，因此debug过程看起来static块的代码并未生效
+		System.out.println(Test.i);
+		System.out.println(Test.j);
+		System.out.println(Test.z);
+	}
+}
